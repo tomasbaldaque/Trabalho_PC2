c1222dc5286185d9369857f65595ac6b65246546
inesdeus <up202307297@up.pt>
2024-05-04 01:52:06 +0100

    adicionei mais algumas coisas, mas não estão a funcionar corretamente ainda, precisam de alguns ajustes

---
+++ subs_login.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request, session
+from classes.userlogin import Userlogin
+
+
+def login():
+    return render_template("login.html", user= "", password="", ulogin=session.get("user"),resul = "")
+
+def logoff():
+    session.pop("user",None)
+    return render_template("index.html", ulogin=session.get("user"))
+
+def chklogin():
+    user = request.form["user"]
+    password = request.form["password"]
+    resul = Userlogin.chk_password(user, password)
+    if resul == "Valid":
+        session["user"] = user
+        return render_template("index.html", ulogin=session.get("user"))
+    return render_template("login.html", user=user, password = password, ulogin=session.get("user"),resul = resul)
