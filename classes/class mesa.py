cec33cf3fb74c8bc63de92198ad06e009f572c5c
antoniooandree <161723446+antoniooandree@users.noreply.github.com>
2024-05-11 21:57:09 +0100

    Add files via upload

---
+++ classes/class mesa.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 11 21:53:48 2024
+
+@author: antoniooandree
+"""
+
+class Gclass:
+    def __init__(self):
+        pass
+
+class Mesa(Gclass):
+    def __init__(self, numero_mesa, capacidade, status):
+        super().__init__()
+        self.__numero_mesa = numero_mesa
+        self.__capacidade = capacidade
+        self.__status = status
+
+    def get_numero_mesa(self):
+        return self.__numero_mesa
+  
+    def set_numero_mesa(self, numero_mesa):
+        self.__numero_mesa = numero_mesa
+   
+    def get_capacidade(self):
+        return self.__capacidade
+  
+    def set_capacidade(self, capacidade):
+        self.__capacidade = capacidade
+    
+    def get_status(self):
+        return self.__status
+ 
+    def set_status(self, status):
+        self.__status = status
+
+
